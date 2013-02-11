57aca7ca59dc599f56ccdf5340a7d37b20505d4a
Arjan Molenaar <gaphor@gmail.com>
2013-02-11 07:20:01 +0100

    Updated message handling. Log level is configurable via ivy.loglevel system property.

--- src/fitnesse/wikitext/widgets/IvyClasspathMessageLogger.java
+++ src/fitnesse/wikitext/widgets/IvyClasspathMessageLogger.java
@@ -7,13 +7,42 @@ import org.apache.ivy.util.Message;
  * A simplified message logger. It's not as verbose as the default message loggers.
  */
 class IvyClasspathMessageLogger extends AbstractMessageLogger {
-    private int level = Message.MSG_WARN;
+    private int level;
 
     /**
      * @param level
      */
     public IvyClasspathMessageLogger() {
+    	final String loglevel = System.getProperty("ivy.loglevel", "i");
+    	setLoglevel(loglevel);
     }
+
+	public void setLoglevel(final String loglevel) {
+    	if (loglevel == null || "".equals(loglevel)) {
+    		level = Message.MSG_INFO;
+    		return;
+    	}
+		switch (loglevel.charAt(0)) {
+    	case 'e':
+    	case 'E':
+    		level = Message.MSG_ERR;
+    		break;
+    	case 'w':
+    	case 'W':
+    		level = Message.MSG_WARN;
+    		break;
+    	case 'v':
+    	case 'V':
+    		level = Message.MSG_VERBOSE;
+    		break;
+    	case 'd':
+    	case 'D':
+    		level = Message.MSG_DEBUG;
+    		break;
+		default:
+			level = Message.MSG_INFO;
+    	}
+	}
 
     public void log(String msg, int level) {
         if (level <= this.level) {
